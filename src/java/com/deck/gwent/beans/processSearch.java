8419a1494eadae1a1336650ce0914d80b555050e
Henry Lin <linm@mail.npu.edu>
2016-04-18 03:37:13 -0700

    added database connectivity to bean

--- src/java/com/deck/gwent/beans/processSearch.java
+++ src/java/com/deck/gwent/beans/processSearch.java
@@ -5,21 +5,28 @@
  */
 package com.deck.gwent.beans;
 
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author Mengchuan Lin
  */
-@ManagedBean
-@RequestScoped
-public class processSearch {
+@ManagedBean (name = "processSearch", eager = false)
+@SessionScoped
+public class processSearch implements Serializable {
 
     private String searchKey;
+    private String result;
     private final String FOLDER = "cards/";
     private final String JPG = ".jpg";
     private String cardImgPath = FOLDER + "gwent-card-backs" + JPG;
@@ -32,14 +39,20 @@ public class processSearch {
     private final List<GwentCard> gwentCardList = new ArrayList<>(Arrays.asList(
              new GwentCard(cardName, cardUnitStrength, cardAbility, cardType)));
 
+    private Connection conn;
+    private PreparedStatement pstmt;
+    private ResultSet rset;
+    private static int count = 0;
+
     /**
      * Creates a new instance of processSearch
      */
     public processSearch() {
+        initDB();
     }
 
     public void setSearchKey(String searchKey) {
-            this.searchKey = searchKey;
+        this.searchKey = searchKey;
     }
 
     public String getSearchKey() {
@@ -47,9 +60,7 @@ public class processSearch {
     }
 
     public String getOutput() {
-        searchKey = searchKey.substring(0, 1).toUpperCase()
-                    + searchKey.substring(1);
-        return "Card Details: " + searchKey;
+        return "Card Details: " + result;
     }
 
     public String getCardImgPath() {
@@ -62,26 +73,57 @@ public class processSearch {
 
     public void runQuery() {
         if (fetchData()) {
+            result = searchKey.substring(0, 1).toUpperCase()
+                     + searchKey.substring(1);
             cardImgPath = FOLDER
                           + searchKey.trim().toLowerCase().replace(' ', '-')
                           + JPG;
-            generateCardDetails();
         }
-        System.out.println("query run");
-    }
-
-    private void generateCardDetails() {
-        cardName = "Yennefer of Vengerberg";
-        cardUnitStrength = "7";
-        cardAbility = "Medic";
-        cardType = "Hero";
-        gwentCardList.clear();
-        gwentCardList.add(new GwentCard(cardName, cardUnitStrength, cardAbility,
-                                        cardType));
+        else {
+            result = "Not found";
+            gwentCardList.clear();
+            gwentCardList.add(new GwentCard("-", "-", "-", "-"));
+            cardImgPath = "error/not-found" + JPG;
+        }
+        searchKey = null; //clear input value in JSF form
+        System.out.println("query #" + ++count + " run");
     }
 
     private boolean fetchData() {
-        return true;
+        try {
+            pstmt.setString(1, searchKey);
+            rset = pstmt.executeQuery();
+
+            if (rset.isBeforeFirst()) {
+                //if cursor is before the first row in result set
+                while (rset.next()) {
+                    cardName = rset.getString("cardName");
+                    cardUnitStrength = rset.getString("unitStrength");
+                    cardAbility = rset.getString("ability");
+                    cardType = rset.getString("cardType");
+                    gwentCardList.clear();
+                    gwentCardList.add(new GwentCard(cardName, cardUnitStrength,
+                                                    cardAbility, cardType));
+                }
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+        return false;
+    }
+
+    private void initDB() {
+        try {
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE",
+                                               "linm", "rommel");
+            System.out.println("Database connection established.");
+            //make preparedStatement
+            pstmt = conn.prepareStatement("SELECT * from basic_gwent"
+                                          + " WHERE cardName = ?");
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
     }
 
     public static class GwentCard {
